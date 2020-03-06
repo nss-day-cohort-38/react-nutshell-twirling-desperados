c5d16a0511e43c51057df287cd1c2a6f601bf599
Mollie Goforth <molliegoforth818@gmail.com>
2020-03-06 15:48:15 -0600

    still not working

--- react-nutshell-twirling-desperados/src/components/tasks/AddTaskForm.js
+++ react-nutshell-twirling-desperados/src/components/tasks/AddTaskForm.js
@@ -1,6 +1,51 @@
 import React, { useState } from 'react';
-import TaskManager from '../../modules/TaskManager';
+import TaskManager from '../../modules/TaskManger';
 
 const AddTaskForm = (props) => {
-    const [task, setTask]
-}
+    const [task, setTask] = useState({ task: "", completionDate: "" });
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleFieldChange = evt => {
+        const stateToChange = { ...task };
+        stateToChange[evt.target.id] = evt.target.value;
+        setTask(stateToChange);
+    };
+    const constructNewTask = evt => {
+        evt.preventDefault();
+        if (task.task == "" || task.completionDate == "") {
+            window.alert("Please enter task and expected completion date");
+        } else {
+            setIsLoading(true);
+            TaskManager.post(task)
+                .then(props.history.push("/tasks"));
+        };
+        return (
+            <>
+                <form>
+                    <fieldset>
+                        <div className="formgrid">
+                            <input type="text"
+                                required
+                                onChange={handleFieldChange}
+                                id="task"
+                                placeholder="taskName" />
+                            <label htmlFor="task">Task</label>
+                            <input type="text"
+                                required
+                                onChange={handleFieldChange}
+                                id="completionDate"
+                                placeholder="completionDate" />
+                            <label htmlFor="completionDate">Completion Date</label>
+                        </div>
+                        <div className="alignRight">
+                            <button type="button"
+                                disabled={isLoading}
+                                onClick={constructNewTask}>Submit</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </>
+        );
+    }
+};
+export default AddTaskForm

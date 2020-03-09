98f0a82bb6b56b1e4ea047a14db9eae77e93ec8f
Mollie Goforth <molliegoforth818@gmail.com>
2020-03-09 15:27:40 -0500

    working on edit form

--- react-nutshell-twirling-desperados/src/components/tasks/EditTaskForm.js
+++ react-nutshell-twirling-desperados/src/components/tasks/EditTaskForm.js
@@ -10,4 +10,61 @@ const handleFieldChange = evt => {
     stateToChange[evt.target.id] = evt.target.value;
     setTask(stateToChange);
 };
-const updateExistingTask =
+const updateExistingTask = evt => {
+evt.preventDefault()
+setIsLoading(true);
+
+const editedTask = {
+    id :props.match.params.taskId,
+    task: task.task,
+    completionDate: task.completionDate
+};
+TaskManger.update(editedTask)
+.then(() =>props.history.push("/tasks"));
+} 
+
+useEffect(()=>{
+    TaskManger.get(props.match.params.taskId)
+    .then(task =>{
+        setTask(task);
+        setIsLoading(true)
+    });
+},[props.match.params.taskId]);
+
+return (
+    <>
+      <form>
+        <fieldset>
+          <div className="formgrid">
+            <input
+              type="text"
+              required
+              className="form-control"
+              onChange={handleFieldChange}
+              id="task"
+              value={task.task}
+            />
+            <label htmlFor="task">Task</label>
+
+            <input
+              type="date"
+              required
+              className="form-control"
+              onChange={handleFieldChange}
+              id="completionDate"
+              value={task.completionDate}
+            />
+            <label htmlFor="completionDate">Completion Date</label>
+          </div>
+          <div className="alignRight">
+            <button
+              type="button" disabled={isLoading}
+              onClick={updateExistingTask}
+              className="btn btn-primary"
+            >Submit</button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+)
+

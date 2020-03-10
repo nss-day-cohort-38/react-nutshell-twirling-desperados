bac34cd5ddf842be69a69371eb639267602ce0e9
Mollie Goforth <molliegoforth818@gmail.com>
2020-03-10 15:12:36 -0500

    all functionality working including removing completed task from list and not from the database

--- react-nutshell-twirling-desperados/src/components/tasks/TaskCard.js
+++ react-nutshell-twirling-desperados/src/components/tasks/TaskCard.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import TaskManger from '../../modules/TaskManger';
 
-
 const TaskCard = props => {
-  const handleFieldChange = ()=>{
+  const handleFieldChange = () => {
     TaskManger.completedTask(props.task)
-    .then(()=> props.getTasks())
+      .then(() => props.getTasks())
   }
   return (
     <div className="taskCard">
-      <div className="taskCard-content">
+      {props.task.isComplete ? null : <div className="taskCard-content">
         <h3>Task: <span className="card-taskName">
           {props.task.task}
         </span></h3>
         <p>Completion Date: {props.task.completionDate}</p>
         <button type="button" onClick={() => props.deleteTask(props.task.id)}>Delete</button>
         <button type="button" onClick={() => props.history.push(`/task/${props.task.id}/edit`)}>Edit</button>
-        <input type="checkbox" 
+        <input type="checkbox"
           required
           className="forms-control"
           id="tasks"
           checked={props.task.isComplete}
-         onChange={handleFieldChange}
-         />
+          onChange={handleFieldChange}
+        />
         <label htmlFor="completed">Completed</label>
-      </div>
+      </div>}
     </div>
   )
 }

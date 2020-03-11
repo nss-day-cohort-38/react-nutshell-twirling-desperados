f38c6844378f792f1aecd599a32d3cd6a18a9c3f
Jack Parsons <jcksnparsons@yahoo.com>
2020-03-11 09:38:24 -0500

    new user form now rejects if any of the fields are blank

--- react-nutshell-twirling-desperados/src/components/auth/NewUserForm.js
+++ react-nutshell-twirling-desperados/src/components/auth/NewUserForm.js
@@ -1,81 +1,87 @@
-import React, { useState } from "react"
-import LoginManager from "../../modules/LoginManager"
+import React, { useState } from "react";
+import LoginManager from "../../modules/LoginManager";
 
+const NewUserForm = props => {
+  const [newUserInfo, setNewUserInfo] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [isAvailable, setIsAvailable] = useState(false);
 
-const NewUserForm = (props) => {
-    const [newUserInfo, setNewUserInfo] = useState({ username: "", email: "", password: "" })
-    const [isAvailable, setIsAvailable] = useState(false)
+  const handleFieldChange = event => {
+    const stateToChange = { ...newUserInfo };
+    stateToChange[event.target.id] = event.target.value;
+    setNewUserInfo(stateToChange);
+  };
 
+  const handleRegistration = event => {
+    event.preventDefault();
 
-    const handleFieldChange = event => {
-        const stateToChange = { ...newUserInfo };
-        stateToChange[event.target.id] = event.target.value;
-        setNewUserInfo(stateToChange)
-    };
+    LoginManager.getUsers().then(arrayOfUsers => {
+      const filteredUsers = arrayOfUsers.filter(
+        element => element.email === newUserInfo.email
+      );
 
+      if (filteredUsers.length !== 0) {
+        window.alert("This is already a registered user!");
+      } else {
+        if (newUserInfo.username === "" || newUserInfo.email === "" || newUserInfo.password === "") {
+          window.alert("You must have a valid email and password! No blanks!");
+        } else {
+          LoginManager.post(newUserInfo).then(() => {
+            LoginManager.getUsers().then(userArray => {
+              const user = userArray.find(
+                el =>
+                  el.email === newUserInfo.email &&
+                  el.password === newUserInfo.password
+              );
+              props.setAsUser(user.id);
+              setIsAvailable(false);
+              props.history.push("/messages");
+            });
+          });
+        }
+      }
+    });
+  };
 
-    const handleRegistration = event => {
-        event.preventDefault()
+  return (
+    <form onSubmit={handleRegistration}>
+      <fieldset>
+        <h3>Add a New User</h3>
+        <div className="formgrid">
+          <input
+            onChange={handleFieldChange}
+            type="text"
+            id="username"
+            required=""
+            autoFocus=""
+          />
+          <label htmlFor="inputUserName">Username</label>
+          <input
+            onChange={handleFieldChange}
+            type="email"
+            id="email"
+            required=""
+            autoFocus=""
+          />
+          <label htmlFor="inputEmail">Email</label>
 
-        LoginManager.getUsers().then(arrayOfUsers => {
-            const filteredUsers = arrayOfUsers.filter(element => element.email === newUserInfo.email)
+          <input
+            onChange={handleFieldChange}
+            type="password"
+            id="password"
+            required=""
+          />
+          <label htmlFor="inputPassword">Password</label>
+        </div>
+        <button disabled={isAvailable} type="submit">
+          Add User
+        </button>
+      </fieldset>
+    </form>
+  );
+};
 
-            if (filteredUsers.length !== 0) {
-                window.alert("This is already a registered user!")
-            } else {
-                LoginManager.post(newUserInfo).then(() => {
-                    LoginManager.getUsers().then(userArray => {
-                        const user = userArray.find(
-                            el =>
-                                el.email === newUserInfo.email &&
-                                el.password === newUserInfo.password
-                        )
-                        props.setAsUser(user.id)
-                        setIsAvailable(false)
-                        props.history.push("/home")
-                    });
-                })
-            }
-        })
-    }
-
-
-
-    return (
-        <form onSubmit={handleRegistration}>
-            <fieldset>
-                <h3>Add a New User</h3>
-                <div className="formgrid">
-                    <input
-                        onChange={handleFieldChange}
-                        type="text"
-                        id="username"
-                        required=""
-                        autoFocus=""
-                    />
-                    <label htmlFor="inputUserName">Username</label>
-                    <input
-                        onChange={handleFieldChange}
-                        type="email"
-                        id="email"
-                        required=""
-                        autoFocus=""
-                    />
-                    <label htmlFor="inputEmail">Email</label>
-
-                    <input
-                        onChange={handleFieldChange}
-                        type="password"
-                        id="password"
-                        required=""
-                    />
-                    <label htmlFor="inputPassword">Password</label>
-                </div>
-                <button disabled={isAvailable} type="submit">Add User</button>
-            </fieldset>
-        </form>
-    )
-
-}
-
-export default NewUserForm
+export default NewUserForm;
